d8eced2a538f9970ee9c6cf66e7159228af88e17
Mathias Gout <mathias.gout@gmail.com>
2021-02-02 22:23:53 +0100

    add checkers

--- main.py
+++ main.py
@@ -10,23 +10,40 @@ class RenameVideoFiles:
         self.full_path = ""
         self.title = ""
         self.lang = ""
+        self.type = ""
+        self.season_number = 0
 
     def _check(self):
         """ Check if attributes are OK """
 
-        self.checked = False
+        self.checked = True
 
-        if not self.full_path:
-            print("Add path (full_path)")
-        if not self.title:
-            print("Add title (title)")
-        if not self.lang:
-            print("Add language (lang)")
+        # Check the path
+        if not os.path.exists(self.full_path):
+            print(f"Add a valid path (full_path), '{self.full_path}' does not exist")
+            self.checked = False
+
+        # Check the title
+        if not self.title or len(self.title) > 100:
+            print("Add a valid title (title)")
+            self.checked = False
         
-        if self.full_path and self.title and self.lang:
-            self.checked=True
+        # Check the language
+        if not self.lang or len(self.lang) > 10:
+            print("Add a valid language (lang)")
+            self.checked = False
 
-    def _get_file_infos(self):
+        # Check the type 
+        if self.type not in ["movie", "serie"]:
+            print("Add a valid video type serie/movie (type)")
+            self.checked = False
+
+        # Check the season number
+        if self.type == "serie" and self.season_number < 1:
+            print("Add a valid season number (season_number)")
+            self.checked = False 
+
+    def _get_files_infos(self):
         """ Get infos about the video file """
 
         self._check()
@@ -36,28 +53,32 @@ class RenameVideoFiles:
             self.height = str(int(cv2video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
             self.width = str(int(cv2video.get(cv2.CAP_PROP_FRAME_WIDTH)))
             self.size_mb = str(int(os.path.getsize(self.full_path) / 10**6))
-            self.video_type = self.full_path.split(".")[-1]
+            self.video_format = self.full_path.split(".")[-1]
 
             self.old_file_name = os.path.basename(self.full_path)
+            self.new_title = "-".join(self.title.split(" ")).lower()
+            self.lang = self.lang.upper()
+            self.season_number = int(self.season_number)
 
     def _rename(self):
         """ Rename the file """
         
         if self.checked:
-            self.new_title = "-".join(self.title.split(" ")).lower()
-            self.new_file_name = self.new_title + "_" + self.width + "x" + self.height + "_" + self.lang + "_" + self.size_mb + "MB" + "." + self.video_type
+            self.new_file_name = self.new_title + "_" + self.width + "x" + self.height + "_" + self.lang + "_" + self.size_mb + "MB" + "." + self.video_format
             print(self.old_file_name)
             print(self.new_file_name)
 
     def rename(self):
         """ Rename method which can be call """
 
-        self._get_file_infos()
+        self._get_files_infos()
         self._rename()
+
 
 if __name__ == "__main__":
     f = RenameVideoFiles()
     f.full_path = "/home/mathias/Documents/videos/Kimi no na wa (2016)/Kimi no Na wa. (Your Name) [BDRip 1920x1080 x264 AAC] VOSTFR V2.mkv"
     f.title = "Kimi no Na wa"
     f.lang = "VOSTFR"
+    f.type = "movie"
     f.rename()
